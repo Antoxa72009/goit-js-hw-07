8162eb659800d4a143b6c0d276d6e72a578c3c2a
Anton_Stepanenko <anton_stepanenko72009@ukr.net>
2025-04-21 14:19:10 +0300

    first commit

---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // зупиняємо перезавантаження сторінки
+
+  const {
+    email: { value: email },
+    password: { value: password },
+  } = form.elements;
+
+  const trimmedEmail = email.trim();
+  const trimmedPassword = password.trim();
+
+  if (trimmedEmail === '' || trimmedPassword === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: trimmedEmail,
+    password: trimmedPassword,
+  };
+
+  console.log(formData);
+  form.reset();
+});
